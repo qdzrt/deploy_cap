a044d865c42859fcb16174fba4dfc9139a2ed791
lianhua <itemyaimydwhg@163.com>
2018-05-17 11:45:45 +0800

    cdp

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,28 +11,10 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 set :pty, true
 set :use_sudo, true
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.4.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl sidekiq sidekiqctl}
-
 append :linked_files, %{config/database.yml config/secrets.yml}
 
 append :linked_dirs, %{bin log tmp/pids tmp/cache tmp/sockets public/system}
 
-# PUMA
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/example.sock"      #根据nginx配置链接的sock进行设置，需要唯一
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, false
-set :puma_preload_app, true #puma使用preload_app!
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -66,3 +48,21 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :rbenv_type, :user
+set :rbenv_ruby, '2.4.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl sidekiq sidekiqctl}
+
+# PUMA
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/example.sock"      #根据nginx配置链接的sock进行设置，需要唯一
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, false
+set :puma_preload_app, true #puma使用preload_app!
