652710decd956d6a9d8f036a5a1f1bfa44b4aa2a
lianhua <itemyaimydwhg@163.com>
2018-05-17 11:18:35 +0800

    capistrano

--- Gemfile
+++ Gemfile
@@ -39,6 +39,12 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'capistrano'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-rails'
+  gem 'capistrano3-puma'
+  gem 'capistrano-sidekiq'
 end
 
 group :development do
---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.2"
+
+set :application, "deploy_cap"
+set :repo_url, "git@github.com:qdzrt/deploy_cap.git"
+
+set :branch, :master
+set :deploy_user, 'root'
+set :deploy_to, "/var/www/#{fetch(:application)}"
+
+set :pty, true
+set :use_sudo, true
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.4.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl sidekiq sidekiqctl}
+
+append :linked_files, %{config/database.yml config/secrets.yml}
+
+append :linked_dirs, %{bin log tmp/pids tmp/cache tmp/sockets public/system}
+
+# PUMA
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/example.sock"      #根据nginx配置链接的sock进行设置，需要唯一
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, false
+set :puma_preload_app, true #puma使用preload_app!
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
